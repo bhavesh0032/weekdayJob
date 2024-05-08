474fd678cc8859911de28b3f5175e27b87d9cffc
Bhavesh Gupta <bhaveshgupta@Bhaveshs-MacBook-Air.local>
2024-05-08 08:55:57 +0530

    final touches removing import statements for elements which are not used

--- src/components/jobcard.js
+++ src/components/jobcard.js
@@ -1,25 +1,9 @@
-// import React from "react";
-// import { Card, CardContent, Typography, Button } from "@mui/material";
-// import './components.css'
-// export const JobCard = ({ job })=> {
-//     return(
-//         <Card className="job-card">
-//       <CardContent>
-//         <Typography variant="h5">{job.companyName}</Typography>
-//         <Typography>{job.jobRole}</Typography>
-//         <Typography>{job.location}</Typography>
-//         <Typography>{job.jobDetailsFromCompany}</Typography>
-//         <Typography>{`Experience: ${job.minExp} - ${job.maxExp}`}</Typography>
-//         <Button variant="contained" color="primary">Apply</Button>
-//       </CardContent>
-//     </Card>
-//     )
-// }
+
 
 import React, { useState } from "react";
 import { Card, CardContent, Typography, Button, Box } from "@mui/material";
 import BoltIcon from "@mui/icons-material/Bolt";
-import CheckBoxIcon from "@mui/icons-material/CheckBox";
+
 import HourglassTopIcon from "@mui/icons-material/HourglassTop";
 
 // import "./components.css";
